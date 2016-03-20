34f2d9660ba7ca2783241f0c5fdb8c5bb720a0df
luke <luke.edwards05@gmail.com>
2016-03-19 21:10:29 -0700

    added threshold option, reworked sequence

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var gzip = require('zlib').gzip;
+var stats = require('fs').statSync;
 var extname = require('path').extname;
 var assign = require('object-assign');
 
 var defaults = {
   extension: '.gz',
+  threshold: false,
   options: {}
 };
 
@@ -12,20 +14,29 @@ module.exports = function () {
 
   return self.filter('gzip', function (data, config) {
     config = assign({}, defaults, config || {});
+    // data = new Buffer('Hello');
     return self.defer(compile.bind(self))(data, config);
   });
 };
 
 function compile(buf, config, cb) {
   var self = this;
-
+  // read the file's extension early
   var ext = extname(config.filename);
-  // modify the file extension, if set
-  var cext = config.extension;
-  if (cext && typeof cext == 'string') {
-    ext += cext;
+
+  // if there is a threshold & we don't exceed it
+  if (config.threshold && typeof config.threshold == 'number') {
+    if (buf.byteLength < config.threshold) {
+      return cb(null, {code: buf, ext: ext});
+    }
   }
 
+  // modify the file extension, if set
+  if (config.extension && typeof config.extension == 'string') {
+    ext += config.extension;
+  }
+
+  // run the gzip module, passing in Gzip options
   return gzip(buf, config.options, function (err, res) {
     if (err) {
       return self.emit('plugin_error', {

eed864a30a533340fc0cba988f3d080822a9915a
luke <luke.edwards05@gmail.com>
2016-03-19 22:15:14 -0700

    add tests!

--- test/test.js
+++ test/test.js
@@ -1,5 +1,37 @@
-const test = require("tape").test
+var test = require('tape').test;
+var gzip = require('../');
 
-test("fly-gzip", (t) => {
-  t.end()
-})
+test('fly-gzip', function (t) {
+	var input = new Buffer('Hello World');
+
+	gzip.call({
+		defer: function (fn) {
+			return function (value, options) {
+				return new Promise(function (resolve, reject) {
+					var cb = function (err, value) {
+						return err ? reject(err) : resolve(value)
+					}
+
+					fn(value, options || cb, options && cb)
+				})
+			}
+		},
+		filter: function (name, fn) {
+			t.plan(4);
+
+			t.equal(name, 'gzip', 'add the fly-gzip filter');
+
+			fn(input).then(function (buf) {
+				t.equal(buf.ext, '.gz', 'add gz extension by default');
+			});
+
+			fn(input, {extension: '.gzip'}).then(function (buf) {
+				t.equal(buf.ext, '.gzip', 'add custom gzip extension');
+			});
+
+			fn(input, {threshold: 5000}).then(function (buf) {
+				t.equal(buf.ext, '', 'will not process if below threshold');
+			});
+		}
+	});
+});

bb78e7cc213c922c2602fa0b14b84bff8a9283b1
Luke Edwards <luke.edwards05@gmail.com>
2016-10-15 08:49:36 +0200

    update plugin logic

--- index.js
+++ index.js
@@ -1,49 +1,29 @@
-var gzip = require('zlib').gzip;
-var stats = require('fs').statSync;
-var assign = require('object-assign');
+'use strict';
 
-var defaults = {
-	extension: '.gz',
+const gzip = require('zlib').gzipSync;
+
+const defaults = {
 	threshold: false,
-	options: {}
+	options: {},
+	ext: 'gz'
 };
 
 module.exports = function () {
-	var self = this;
+	this.plugin('gzip', {}, function * (file, opts) {
+		opts = Object.assign({}, defaults, opts);
 
-	return self.filter('gzip', function (data, config) {
-		config = assign({}, defaults, config || {});
-		// data = new Buffer('Hello');
-		return self.defer(compile.bind(self))(data, config);
+		// if there is a threshold && we don't meet it, exit
+		if (typeof opts.threshold === 'number' && Buffer.byteLength(file.data) < opts.threshold) {
+			return;
+		}
+
+		// clone the file object
+		let clone = Object.assign({}, file);
+
+		// modify the file extension
+		clone.base += (opts.ext.charAt(0) === '.') ? opts.ext : `.${opts.ext}`;
+
+		// compress & set data
+		clone.data = gzip(clone.data, opts.options);
 	});
 };
-
-function compile(buf, config, cb) {
-	var self = this;
-	var ext = config.file.ext || ''; // eg: '.js'
-
-	// if there is a threshold & we don't exceed it
-	if (config.threshold && typeof config.threshold == 'number') {
-
-		if (Buffer.byteLength(buf) < config.threshold) {
-			return cb(null, {code: buf, ext: ext});
-		}
-	}
-
-	// modify the file extension, if set
-	if (config.extension && typeof config.extension == 'string') {
-		ext += config.extension;
-	}
-
-	// run the gzip module, passing in Gzip options
-	return gzip(buf, config.options, function (err, res) {
-		if (err) {
-			return self.emit('plugin_error', {
-				plugin: 'fly-gzip',
-				error: err.message
-			});
-		}
-
-		cb(null, {code: res, ext: ext});
-	});
-}

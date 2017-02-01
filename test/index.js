39068ad82f420216480b093d2d72afab52b97b75
Luke Edwards <luke.edwards05@gmail.com>
2017-01-31 16:43:10 -0800

    update tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const unzip = require('zlib').gunzipSync;
-const join = require('path').join;
-const test = require('tape').test;
+const {join} = require('path');
+const test = require('tape');
 const Fly = require('fly');
 
 const tmp = join(__dirname, 'tmp');
@@ -12,37 +12,38 @@ test('fly-gzip', t => {
 	t.plan(6);
 
 	const fly = new Fly({
-		plugins: [{
-			func: require('../')
-		}],
+		plugins: [
+			require('../'),
+			require('fly-clear')
+		],
 		tasks: {
-			a: function * () {
-				yield this.source(file).gzip().target(tmp);
+			* foo(f) {
+				yield f.source(file).gzip().target(tmp);
 
-				const arr = yield this.$.expand(`${tmp}/*.*`);
-				const str = yield this.$.read(`${tmp}/a.txt.gz`);
+				const arr = yield f.$.expand(`${tmp}/*.*`);
+				const str = yield f.$.read(`${tmp}/a.txt.gz`);
 				t.equal(arr.length, 2, 'create two files');
 				t.ok(str, 'use default `gz` extension');
 				t.equal(unzip(str).toString(), 'Hello world!\n', 'can decompress the file correctly');
 
-				yield this.clear(tmp);
+				yield f.clear(tmp);
 			},
-			b: function * () {
-				yield this.source(file).gzip({threshold: 1024}).target(tmp);
-				const arr = yield this.$.expand(`${tmp}/*.*`);
+			* bar(f) {
+				yield f.source(file).gzip({threshold: 1024}).target(tmp);
+				const arr = yield f.$.expand(`${tmp}/*.*`);
 				t.equal(arr.length, 1, 'do not use `gzip` if file is too small');
-				yield this.clear(tmp);
+				yield f.clear(tmp);
 			},
-			c: function * () {
-				yield this.source(file).gzip({ext: 'fake'}).target(tmp);
-				const arr = yield this.$.expand(`${tmp}/*.fake`);
+			* baz(f) {
+				yield f.source(file).gzip({ext: 'fake'}).target(tmp);
+				const arr = yield f.$.expand(`${tmp}/*.fake`);
 				t.equal(arr.length, 1, 'pass a custom extension');
-				yield this.clear(tmp);
+				yield f.clear(tmp);
 			}
 		}
 	});
 
-	t.ok('gzip' in fly, 'attach `gzip` function to Fly');
+	t.ok('gzip' in fly.plugins, 'attach `gzip` function to Fly');
 
-	fly.serial(['a', 'b', 'c']);
+	fly.serial(['foo', 'bar', 'baz']);
 });
